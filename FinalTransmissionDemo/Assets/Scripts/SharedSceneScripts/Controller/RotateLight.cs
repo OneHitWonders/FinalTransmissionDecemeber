f610c1a7d15d57770d95494c1e4b7bd4c9447cd0
Peter Casey <s00162137@mail.itsligo.ie>
2018-02-07 10:11:47 +0000

    MinorChanges
    Night time light, Can set day start time in inspector, added pause menu panel(still to be coded) Building Search Added

---
+++ FinalTransmissionDemo/Assets/Scripts/SharedSceneScripts/Controller/RotateLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour {
+
+    //0.001388888889f rotateAround Value for 12 min rotation
+
+    public float rotationAngle = 0.001388888889f;
+
+
+    public Vector3 defaultPosition;
+    public Quaternion defaultRotation;
+	// Use this for initialization
+	void Awake () {
+        GameObject controller = GameObject.FindGameObjectWithTag("GameController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.RotateAround(Vector3.zero, Vector3.forward, rotationAngle);
+        gameObject.transform.LookAt(Vector3.zero);
+	}
+}
--- FinalTransmissionDemo/Assets/Scripts/TestSpecificScripts/SearchBuilding.cs
+++ FinalTransmissionDemo/Assets/Scripts/TestSpecificScripts/SearchBuilding.cs
@@ -4,39 +4,147 @@ using UnityEngine;
 
 public class SearchBuilding : MonoBehaviour {
 
-    private bool canSearch = false;
+    private ResourceCollector resourceCollector;
+
     private SurvivorController survController;
     private GameObject survivorOutside;
 
+    [SerializeField]
+    private int BuildingSize =1 ;
+
+
+    //Variables for Searching
+    private bool canSearch = false;
+    private bool beingSearched = false;
+    private DayTime dayTime;
+    private float HrFinishSearch = 0;
+
+    private bool BeenSearched = false;
+
+
 	// Use this for initialization
 	void Start () {
-        survController = GameObject.FindGameObjectWithTag("GameController").GetComponent<SurvivorController>();
-	}
+        survController = GameObject.FindGameObjectWithTag("SceneController").GetComponent<SurvivorController>();
+        dayTime = GameObject.FindGameObjectWithTag("SceneController").GetComponent<DayTime>();
+        resourceCollector = GameObject.FindGameObjectWithTag("SceneController").GetComponent<ResourceCollector>();
+
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if ((canSearch == true) && survController.selectedSurvivor)
+        if ((canSearch == true) && BeenSearched == false)
         {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                BeginBuildingSearch();
+            }
+        }
 
+        //When Search Timer Ends
+        if (beingSearched == true)
+        {
+            if (HrFinishSearch == dayTime.hour)
+            {
+                FinishedSearch();
+            }
         }
 	}
 
 
+    public void BeginBuildingSearch()
+    {
+        survivorOutside.SetActive(false);
+        beingSearched = true;
+        HrFinishSearch = dayTime.hour + 2;
+        Debug.Log(HrFinishSearch.ToString());
+    }
+
+    private void FinishedSearch()
+    {
+        beingSearched = false;
+        BeenSearched = true; // prevents re-searching
+        survivorOutside.SetActive(true);
+        GenerateResourcesGathered();
+
+
+    }
+
+    public void GenerateResourcesGathered()
+    {
+        //Food
+        if (Random.Range(0,10) <= 7)
+        {
+            resourceCollector.foodCollected += (4 * BuildingSize) + 2;
+
+        }
+        //Water
+        if (Random.Range(0, 10) <= 8)
+        {
+            resourceCollector.waterCollected += (3 * BuildingSize) + 2;
+
+        }
+        //Wood
+        if (Random.Range(0, 10) <= 6)
+        {
+            resourceCollector.woodCollected += (3 * BuildingSize) + 2;
+
+        }
+        //Metal
+        if (Random.Range(0, 10) <= 4)
+        {
+            resourceCollector.metalCollected += (2 * BuildingSize) + 2;
+
+        }
+        //Ammo
+        if (Random.Range(0, 10) <= 3)
+        {
+            resourceCollector.ammoCollected += (2 * BuildingSize) + 2;
+
+        }
+        //Money
+        if (Random.Range(0, 10) <= 4)
+        {
+            resourceCollector.moneyCollected += (2 * BuildingSize) + 2;
+
+        }
+        Debug.Log("ResourceGenerationCalled");
+    }
+
+
+
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == survController.selectedSurvivor)
+        if (BeenSearched == false)
         {
-            canSearch = true;
+            if ((other.gameObject.tag == "Survivor") && beingSearched == false)
+            {
+                survivorOutside = other.gameObject;
+                canSearch = true;
+                Debug.Log("Can search");
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Survivor")
-        {
-            canSearch = false;
+       
+            if (other.tag == "Survivor")
+            {
 
-        }
+                survivorOutside = null;
+                canSearch = false;
+            Debug.Log("TriggerLeave");
+            }
+        
+
     }
+
+
+
+
+
 
 }
